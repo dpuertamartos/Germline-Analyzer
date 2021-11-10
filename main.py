2b1a4950aaa358b989a698fb165761140f2a35aa
David Puerta Martos <dpuerta@usal.es>
2021-11-10 20:38:49 +0100

    Improved algorithm to take exactly 50 points, last point is an average of variable size(from 0 to 2%) to compensate not being exactly 2% steps, due to not round number of pixels

--- main.py
+++ main.py
@@ -20,7 +20,18 @@ def getlist(file):
     df = pd.read_csv(file)
     stand_intensity(df)
     stand_length(df)
-    #divide the number of points to take 50 points
+
+    #Create the points to take list. Points selected are the ones closer to the 2% step change. For example 1.98% if next one is 2.01%.
+    #From these points we will take the previous ~2% pixels and make an average
+    points_to_take = []
+
+    for i, percentage in enumerate(df["Distance_(pixels)"].tolist()):
+        if i+1 == len(df["Distance_(pixels)"].tolist()):
+            points_to_take[-1]=i
+        elif int(df["Distance_(pixels)"].tolist()[i+1]) > int(df["Distance_(pixels)"].tolist()[i]) and int(df["Distance_(pixels)"].tolist()[i]) % 2 != 0:
+            points_to_take.append(i)
+
+    #divide the number of pixels between 50
     window50 = int(len(df.Gray_Value)/50)
     #create the rolling average taking the previous 2% of pixels
     roll_df = df.rolling(window=window50).mean()
@@ -28,10 +39,8 @@ def getlist(file):
     roll_df_list = roll_df["Gray_Value"].tolist()
     #Create a 50 values list (gray value average each 2% of the germline)
     #For this it takes the rolling average value each (window50) pixels
-    point_list_50 = [value for x, value in enumerate(roll_df_list) if (x+1) % window50 == 0]
+    point_list_50 = [round(value,2) for x, value in enumerate(roll_df_list) if x in points_to_take]
     print(point_list_50)
-    print(len(roll_df_list)/window50)
-    print(len(point_list_50))
 
 
 entries = os.listdir('Values/')
