dd3a192b58762d4b3a9bcfc6bb13366f7367b8db
David Puerta Martos <dpuerta@usal.es>
2021-11-10 20:10:27 +0100

    Take multiple csv files from Values folder

--- main.py
+++ main.py
@@ -1,40 +1,44 @@
 import pandas as pd
+import os
 
-# TODO 1: Full germline image , make the line in imageJ
+# 1: Full germline image , make the line in imageJ
 # Manual
-# TODO 2: imageJ plot profile
+# 2: imageJ plot profile
 # Manual width 50 segmented line from mid of first distal cell to mid of first unique cell of proximal gonad
 # Generate .csv files (1 per germline)
-# TODO 3: standarize to 100% intensity
-df = pd.read_csv("Values1.csv")
 
-def stand_intensity(dataframe):
-    dataframe.Gray_Value = dataframe.Gray_Value * 100 / dataframe.Gray_Value.max()
+def getlist(file):
+    #function that standarize intensity
+    def stand_intensity(dataframe):
+        dataframe.Gray_Value = dataframe.Gray_Value * 100 / dataframe.Gray_Value.max()
+
+    #function that standarize lenght
+    def stand_length(dataframe):
+        dataframe["Distance_(pixels)"] = dataframe["Distance_(pixels)"] * 100 / dataframe["Distance_(pixels)"].max()
 
 
-stand_intensity(df)
+    df = pd.read_csv(file)
+    stand_intensity(df)
+    stand_length(df)
+    #divide the number of points to take 50 points
+    window50 = int(len(df.Gray_Value)/50)
+    #create the rolling average taking the previous 2% of pixels
+    roll_df = df.rolling(window=window50).mean()
+    #take the dataframe Gray Value and convert it to a list
+    roll_df_list = roll_df["Gray_Value"].tolist()
+    #Create a 50 values list (gray value average each 2% of the germline)
+    #For this it takes the rolling average value each (window50) pixels
+    point_list_50 = [value for x, value in enumerate(roll_df_list) if (x+1) % window50 == 0]
+    print(point_list_50)
+    print(len(roll_df_list)/window50)
+    print(len(point_list_50))
 
-# TODO 4: standarize to 100% lenght
 
-def stand_length(dataframe):
-    dataframe["Distance_(pixels)"] = dataframe["Distance_(pixels)"] * 100 / dataframe["Distance_(pixels)"].max()
+entries = os.listdir('Values/')
+print(entries)
 
-stand_length(df)
-
-# TODO 5: make the rolling average 2%
-#divide the number of points to take 50 points
-window50 = int(len(df.Gray_Value)/50)
-
-#create the rolling average taking the previous 2% of pixels
-roll_df = df.rolling(window=window50).mean()
-#take the dataframe Gray Value and convert it to a list
-roll_df_list = roll_df["Gray_Value"].tolist()
-
-#Create a 50 values list (gray value average each 2% of the germline)
-#For this it takes the rolling average value each (window50) pixels
-
-point_list_50 = [value for x, value in enumerate(roll_df_list) if (x+1) % 30 == 0]
-
+for file in entries:
+    getlist(f'Values/{file}')
 
 # TODO 6: be able to aggregate multiple germlines
 
