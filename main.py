bdf4c020da755bd960f7c1313d1a23b17a8ba73b
David Puerta Martos <dpuerta@usal.es>
2021-11-10 19:53:35 +0100

    Opens excel as a pandas dataframe. It's able to standerize fluorescence intensity and gonad length. Creates a 50 point list with the average values each 2% of gonad.

--- main.py
+++ main.py
@@ -1,15 +1,43 @@
-#TODO 1: Full germline image , make the line in imageJ
+import pandas as pd
 
-#TODO 2: imageJ plot profile
+# TODO 1: Full germline image , make the line in imageJ
+# Manual
+# TODO 2: imageJ plot profile
+# Manual width 50 segmented line from mid of first distal cell to mid of first unique cell of proximal gonad
+# Generate .csv files (1 per germline)
+# TODO 3: standarize to 100% intensity
+df = pd.read_csv("Values1.csv")
 
-#TODO 3: standarize to 100% intensity
+def stand_intensity(dataframe):
+    dataframe.Gray_Value = dataframe.Gray_Value * 100 / dataframe.Gray_Value.max()
 
-#TODO 4: standarize to 100% lenght
 
-#TODO 5: make the rolling average 2%
+stand_intensity(df)
 
-#TODO 6: be able to aggregate multiple germlines
+# TODO 4: standarize to 100% lenght
 
-#TODO 7: show graph and descriptive statistics
+def stand_length(dataframe):
+    dataframe["Distance_(pixels)"] = dataframe["Distance_(pixels)"] * 100 / dataframe["Distance_(pixels)"].max()
 
-#TODO 8: make it a webapp with flask
+stand_length(df)
+
+# TODO 5: make the rolling average 2%
+#divide the number of points to take 50 points
+window50 = int(len(df.Gray_Value)/50)
+
+#create the rolling average taking the previous 2% of pixels
+roll_df = df.rolling(window=window50).mean()
+#take the dataframe Gray Value and convert it to a list
+roll_df_list = roll_df["Gray_Value"].tolist()
+
+#Create a 50 values list (gray value average each 2% of the germline)
+#For this it takes the rolling average value each (window50) pixels
+
+point_list_50 = [value for x, value in enumerate(roll_df_list) if (x+1) % 30 == 0]
+
+
+# TODO 6: be able to aggregate multiple germlines
+
+# TODO 7: show graph and descriptive statistics
+
+# TODO 8: make it a webapp with flask
