e9394579124305c4141a37a6680696da6c1cdbb3
David Puerta Martos <dpuerta@usal.es>
2021-11-10 18:39:52 +0100

    First TODO List

---
+++ main.py
@@ -0,0 +1,15 @@
+#TODO 1: Full germline image , make the line in imageJ
+
+#TODO 2: imageJ plot profile
+
+#TODO 3: standarize to 100% intensity
+
+#TODO 4: standarize to 100% lenght
+
+#TODO 5: make the rolling average 2%
+
+#TODO 6: be able to aggregate multiple germlines
+
+#TODO 7: show graph and descriptive statistics
+
+#TODO 8: make it a webapp with flask
