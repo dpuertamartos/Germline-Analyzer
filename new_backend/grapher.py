e22c89bafa74af40e643572ddbbcbf7aea375ab9
David Puerta Martos <dpuerta@usal.es>
2022-04-12 13:18:44 +0200

    grapher function can plot one or multiple strains

--- new_backend/grapher.py
+++ new_backend/grapher.py
@@ -1,11 +1,14 @@
 import matplotlib.pyplot as plt
 
-def plotGermline(df):
-    v = 100 / len(df.average)
-    strain = "PRUEBA"
+def plotGermline(df, title="no title", strain_name_list=["NO TITLE"],file_namelist_list=["None"]):
+    v = 100 / len(df[0].average)
     fig = plt.figure()
     axis = fig.add_subplot(1, 1, 1)
-    axis.set_title(f'{strain}   (n = {len(df.columns)-2})')
-    axis.errorbar(df.index * v, df.average, df.stddev, linestyle=':', marker='^', capsize=3,
-                  elinewidth=0.7)
+    axis.set_title(f'{title}')
+    for i, df in enumerate(df):
+        axis.errorbar(df.index * v, df.average, df.stddev,
+                      label=f'{strain_name_list[i]} n={len(file_namelist_list[i])}', linestyle=':', marker='^',
+                      capsize=3,
+                      elinewidth=0.7)
+    axis.legend()
     plt.show()
