6659f32f49d0fdc3698f0f79ad6cd3b2f1ae03c8
dpuertamartos <92330952+dpuertamartos@users.noreply.github.com>
2022-12-11 00:35:28 +0100

    Update main.py
    fixed commit mess

--- main.py
+++ main.py
@@ -37,7 +37,6 @@ def index():
 @app.route('/multiplestrains/<int:strainnumber>', methods=['GET', 'POST'])
 def multiplestrains_plot(strainnumber):
     if request.method == 'POST':
-        # TODO OPTIONAL: Store dataframes in database then retrieve them for each plot configuration
         # TODO 3: Fix get starter guide
         # TODO 4: Reorganize imports , .env, .gitignore and all
         # TODO OPTIONAL: Organize dataframes stored per user
@@ -118,16 +117,12 @@ def plot(strains):
     #TODO 1: LET USER PERMANTENT STORE THEIR DATA
     #TODO 2: FIX MOBILE INTERFACE(NUMBER OF STRAINS SELECTOR AND POSS OTHER)
     #TODO 3: CLEAN CODE
-    #TODO 4: BUG WHEN YOU CHOOSE MITOTIC ZONE OPTION, THEY WILL ALL HAVE THE SAME NAME
     #TODO 5: IMPROVE AESTHETIC OF GONAD LENGTH
-    #TODO 9: CHANGE PLOT TO SHOW ABSOLUTE UNITS (pixels, micros, cell diameters)
-    #OF FIRST STRAIN IN THE MITOTIC ZONE LOAD DATA WINDOW
 
     id = session.get("id")
     mitotic_graph_info = session.get("mitotic_zone")
     mitotic_graph_error = session.get("mitotic_zone_error")
     mitotic_files_loaded = session.get("mitotic_mode") == "True"
-    print("retrieved mitotic graph info", mitotic_graph_info, mitotic_graph_error)
     strain_name_list = session.get("strain_name_list")
     files_list_list = session.get("files_list_list")
     print("retrieving session", strain_name_list,files_list_list)
@@ -157,6 +152,8 @@ def plot(strains):
         abs_switch = request.form.get('flexswitchabs')
         mitotic_switched_on = option_switch == "on"
         abs_switched_on = abs_switch == "on"
+        if abs_switched_on:
+            mitotic_switched_on = False
         abs = None
         to_show_abs = int(min_length)
         if abs_switched_on:
@@ -175,7 +172,19 @@ def plot(strains):
         range_start = per_fld.split(" - ")[0]
         range_end = per_fld.split(" - ")[1]
         range_array = [int(range_start) / 100, int(range_end) / 100]
-        print("per_fld", per_fld, range_start, range_end, range_array)
+        option2 = request.form.getlist('flexRadioLength')
+        px = option2[0] == "px"
+        mc = option2[0] == "mc"
+        gcd = option2[0] == "gcd"
+        convert_ratio = float(request.form.get("convert_ratio"))
+        convert_ratio_finale = None
+        if px:
+            convert_ratio_finale = 1
+        elif mc:
+            convert_ratio_finale = convert_ratio
+        elif gcd:
+            #micron to gcd ratio is hardcoded as 1/3.5
+            convert_ratio_finale = convert_ratio / 3.5
 
         # flash error if range for fold increase is not enough for 1 point
         if fld and int(int(points) * (range_array[1] - range_array[0])) == 0:
@@ -190,14 +199,16 @@ def plot(strains):
                                     percentage_for_fold_increase=range_array, absolute_length=abs)
 
             list_of_dataframes_processed.append(germline.process())
-            print("germline length", germline.return_max_length())
+            
 
-        fig = plotGermline(list_of_dataframes_processed, title="PRUEBA",
+        fig = plotGermline(list_of_dataframes_processed, title="",
                            strain_name_list=strain_name_list,
                            file_namelist_list=files_list_list,
                            mitotic_mode=mitotic_switched_on,
                            strains_mitotic_percentage=mitotic_graph_info, strains_error=mitotic_graph_error,
-                           dpi=dpi, average_length=average_length)
+                           dpi=dpi, average_length=average_length, absolute_cut=abs,
+                           conversion=convert_ratio_finale,
+                           x_label=[px, mc, gcd], y_label=[std, fld, [range_start, range_end]])
         png = convert_plot_to_png(fig)
         b64 = encode_png_to_base64(png)
 
@@ -208,14 +219,14 @@ def plot(strains):
                                can_absolute_length=can_absolute_length, absolute_length_selected = abs_switched_on,
                                npoints=int(points), range_fold=per_fld,
                                range_fold_1=int(range_start), range_fold_2=int(range_end), std=std, fld=fld, dpi=dpi,
-                               mitotic=mitotic_files_loaded, mitotic_switched_on=mitotic_switched_on)
+                               mitotic=mitotic_files_loaded, mitotic_switched_on=mitotic_switched_on, px=px, mc=mc, gcd=gcd, convert_ratio=convert_ratio)
 
     return render_template('plot.html', strains=strains, files_list_list=files_list_list,strain_name_list=strain_name_list,
                            lengths_list_list=final_length_list, min_length=min_length, current_length=min_length,
                            average_length=average_length,
                            can_absolute_length=can_absolute_length, absolute_length_selected=False,
                            npoints=50, range_fold="0-4", range_fold_1=0, range_fold_2=4, std=False, fld=False, dpi=100,
-                           mitotic=mitotic_files_loaded, mitotic_switched_on=True)
+                           mitotic=mitotic_files_loaded, mitotic_switched_on=True, px=False, mc=False, gcd=False, convert_ratio=0.22)
 
 @app.route('/trial', methods=['GET'])
 def trial():
